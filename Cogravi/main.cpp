035e8cba8e7f8762a4a4dca2437c8e0b5e34f23f
Yordy Leonidas <yordy.lmv.2000@gmail.com>
2020-10-29 21:45:15 -0500

    usando opencv para cargar videos

--- Cogravi/main.cpp
+++ Cogravi/main.cpp
@@ -1,280 +1,270 @@
-﻿//#include <GL/glew.h>
-//#include <GLFW/glfw3.h>
-////#include <stb_image.h>
-//
-//#define STB_IMAGE_IMPLEMENTATION
-//#include <stb_image.h>
-//
-//#include <glm/glm.hpp>
-//#include <glm/gtc/matrix_transform.hpp>
-//#include <glm/gtc/type_ptr.hpp>
-//
-//#include <iostream>
-//
-//using namespace std;
-//
-//
-//#include "Shader.h"
-//#include "Util.h"
-//#include "Camera.h"
-//#include "GameObject.h"
-//
-//#include <iostream>
-//
-//void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-//void mouse_callback(GLFWwindow* window, double xpos, double ypos);
-//void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
-//void processInput(GLFWwindow* window);
-//
-//// settings
-//const unsigned int SCR_WIDTH = 800;
-//const unsigned int SCR_HEIGHT = 600;
-//
-//// camera
-//Camera camera(glm::vec3(0.0f, 0.0f, 155.0f));
-//float lastX = (float)SCR_WIDTH / 2.0;
-//float lastY = (float)SCR_HEIGHT / 2.0;
-//bool firstMouse = true;
-//
-//// timing
-//float deltaTime = 0.0f;
-//float lastFrame = 0.0f;
-//
-//using namespace Cogravi;
-//
-//int main()
-//{
-//    // glfw: initialize and configure
-//    // ------------------------------
-//    glfwInit();
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-//    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-//    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-//
-//#ifdef __APPLE__
-//    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-//#endif
-//
-//    // glfw window creation
-//    // --------------------
-//    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
-//    if (window == NULL)
-//    {
-//        std::cout << "Failed to create GLFW window" << std::endl;
-//        glfwTerminate();
-//        return -1;
-//    }
-//    glfwMakeContextCurrent(window);
-//    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-//    glfwSetCursorPosCallback(window, mouse_callback);
-//    glfwSetScrollCallback(window, scroll_callback);
-//
-//    // tell GLFW to capture our mouse
-//    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-//
-//    glfwSwapInterval(0);
-//
-//    // glad: load all OpenGL function pointers
-//    // ---------------------------------------
-//    if (GLenum err = glewInit()) return 0;
-//
-//    // configure global opengl state
-//    // -----------------------------
-//    glEnable(GL_DEPTH_TEST);
-//
-//    // build and compile shaders
-//    // -------------------------
-//    Shader asteroidShader("assets/shaders/10.3.asteroids.vs", "assets/shaders/10.3.asteroids.fs");
-//    Shader planetShader("assets/shaders/10.3.planet.vs", "assets/shaders/10.3.planet.fs");
-//
-//    // load models
-//    // -----------
-//    GameObject rock(glm::vec3(0.0f), glm::vec3(0.0f), glm::vec3(1.0f), "assets/objects/rock/rock.obj");
-//    GameObject planet(glm::vec3(10.0f), glm::vec3(0.0f), glm::vec3(1.0f), "assets/objects/planet/planet.obj");
-//
-//
-//    // generate a large list of semi-random model transformation matrices
-//    // ------------------------------------------------------------------
-//    unsigned int amount = 300;
-//    glm::mat4* modelMatrices;
-//    modelMatrices = new glm::mat4[amount];
-//    srand(glfwGetTime()); // initialize random seed	
-//    float radius = 150.0;
-//    float offset = 25.0f;
-//    for (unsigned int i = 0; i < amount; i++)
-//    {
-//        glm::mat4 model = glm::mat4(1.0f);
-//        // 1. translation: displace along circle with 'radius' in range [-offset, offset]
-//        float angle = (float)i / (float)amount * 360.0f;
-//        float displacement = (rand() % (int)(2 * offset * 100)) / 100.0f - offset;
-//        float x = sin(angle) * radius + displacement;
-//        displacement = (rand() % (int)(2 * offset * 100)) / 100.0f - offset;
-//        float y = displacement * 0.4f; // keep height of asteroid field smaller compared to width of x and z
-//        displacement = (rand() % (int)(2 * offset * 100)) / 100.0f - offset;
-//        float z = cos(angle) * radius + displacement;
-//        model = glm::translate(model, glm::vec3(x, y, z));
-//
-//        // 2. scale: Scale between 0.05 and 0.25f
-//        float scale = (rand() % 20) / 100.0f + 0.05;
-//        model = glm::scale(model, glm::vec3(scale));
-//
-//        // 3. rotation: add random rotation around a (semi)randomly picked rotation axis vector
-//        float rotAngle = (rand() % 360);
-//        model = glm::rotate(model, rotAngle, glm::vec3(0.4f, 0.6f, 0.8f));
-//
-//        // 4. now add to list of matrices
-//        modelMatrices[i] = model;
-//    }
-//
-//    // configure instanced array
-//    // -------------------------
-//    unsigned int buffer;
-//    glGenBuffers(1, &buffer);
-//    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-//    glBufferData(GL_ARRAY_BUFFER, amount * sizeof(glm::mat4), &modelMatrices[0], GL_STATIC_DRAW);
-//
-//    // set transformation matrices as an instance vertex attribute (with divisor 1)
-//    // note: we're cheating a little by taking the, now publicly declared, VAO of the model's mesh(es) and adding new vertexAttribPointers
-//    // normally you'd want to do this in a more organized fashion, but for learning purposes this will do.
-//    // -----------------------------------------------------------------------------------------------------------------------------------
-//    for (unsigned int i = 0; i < rock.meshes.size(); i++)
-//    {
-//        unsigned int VAO = rock.meshes[i].VAO;
-//        glBindVertexArray(VAO);
-//        // set attribute pointers for matrix (4 times vec4)
-//        glEnableVertexAttribArray(3);
-//        glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)0);
-//        glEnableVertexAttribArray(4);
-//        glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(sizeof(glm::vec4)));
-//        glEnableVertexAttribArray(5);
-//        glVertexAttribPointer(5, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(2 * sizeof(glm::vec4)));
-//        glEnableVertexAttribArray(6);
-//        glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(3 * sizeof(glm::vec4)));
-//
-//        glVertexAttribDivisor(3, 1);
-//        glVertexAttribDivisor(4, 1);
-//        glVertexAttribDivisor(5, 1);
-//        glVertexAttribDivisor(6, 1);
-//
-//        glBindVertexArray(0);
-//    }
-//
-//    // render loop
-//    // -----------
-//    while (!glfwWindowShouldClose(window))
-//    {
-//        // per-frame time logic
-//        // --------------------
-//        float currentFrame = glfwGetTime();
-//        deltaTime = currentFrame - lastFrame;
-//        lastFrame = currentFrame;
-//
-//        // input
-//        // -----
-//        processInput(window);
-//
-//        // render
-//        // ------
-//        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-//        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-//
-//        // configure transformation matrices
-//        glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 1000.0f);
-//        glm::mat4 view = camera.GetViewMatrix();
-//        asteroidShader.use();
-//        asteroidShader.setMat4("projection", projection);
-//        asteroidShader.setMat4("view", view);
-//        planetShader.use();
-//        planetShader.setMat4("projection", projection);
-//        planetShader.setMat4("view", view);
-//
-//        // draw planet
-//        glm::mat4 model = glm::mat4(1.0f);
-//        model = glm::translate(model, glm::vec3(0.0f, -3.0f, 0.0f));
-//        model = glm::scale(model, glm::vec3(4.0f, 4.0f, 4.0f));
-//        planetShader.setMat4("model", model);
-//        planet.draw(planetShader);
-//
-//        // draw meteorites
-//        asteroidShader.use();
-//        asteroidShader.setInt("texture_diffuse1", 0);
-//        glActiveTexture(GL_TEXTURE0);
-//        glBindTexture(GL_TEXTURE_2D, rock.textures_loaded[0].id); // note: we also made the textures_loaded vector public (instead of private) from the model class.
-//        for (unsigned int i = 0; i < rock.meshes.size(); i++)
-//        {
-//            glBindVertexArray(rock.meshes[i].VAO);
-//            glDrawElementsInstanced(GL_TRIANGLES, rock.meshes[i].indices.size(), GL_UNSIGNED_INT, 0, amount);
-//            glBindVertexArray(0);
-//        }
-//
-//        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
-//        // -------------------------------------------------------------------------------
-//        glfwSwapBuffers(window);
-//        glfwPollEvents();
-//    }
-//
-//    glfwTerminate();
-//    return 0;
-//}
-//
-//// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
-//// ---------------------------------------------------------------------------------------------------------
-//void processInput(GLFWwindow* window)
-//{
-//    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-//        glfwSetWindowShouldClose(window, true);
-//
-//    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-//        camera.ProcessKeyboard(FORWARD, deltaTime);
-//    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-//        camera.ProcessKeyboard(BACKWARD, deltaTime);
-//    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-//        camera.ProcessKeyboard(LEFT, deltaTime);
-//    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-//        camera.ProcessKeyboard(RIGHT, deltaTime);
-//}
-//
-//// glfw: whenever the window size changed (by OS or user resize) this callback function executes
-//// ---------------------------------------------------------------------------------------------
-//void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-//{
-//    // make sure the viewport matches the new window dimensions; note that width and 
-//    // height will be significantly larger than specified on retina displays.
-//    glViewport(0, 0, width, height);
-//}
-//
-//// glfw: whenever the mouse moves, this callback is called
-//// -------------------------------------------------------
-//void mouse_callback(GLFWwindow* window, double xpos, double ypos)
-//{
-//    if (firstMouse)
-//    {
-//        lastX = xpos;
-//        lastY = ypos;
-//        firstMouse = false;
-//    }
-//
-//    float xoffset = xpos - lastX;
-//    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
-//
-//    lastX = xpos;
-//    lastY = ypos;
-//
-//    camera.ProcessMouseMovement(xoffset, yoffset);
-//}
-//
-//// glfw: whenever the mouse scroll wheel scrolls, this callback is called
-//// ----------------------------------------------------------------------
-//void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
-//{
-//    camera.ProcessMouseScroll(yoffset);
-//}
+﻿#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include <iostream>
+
+#include <glm/glm.hpp>
+#include "Shader.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+using namespace Cogravi;
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/video.hpp>
+
+using namespace cv;
 
 
-#include "Application.h"
+
+
+Mat CapturedImage;
+
+class CaptureClient
+{
+
+private:
+    VideoCapture Capture;
+    int ActiveChannel;
+    const int MaxChannels;
+
+public:
+    bool ThreadEnabled;
+
+    CaptureClient() : ActiveChannel(0), MaxChannels(2), ThreadEnabled(true) {}
+
+    void SwitchChannel() {
+
+        // switch to next active channel
+        if (ActiveChannel >= MaxChannels) {
+            ActiveChannel = 1;
+        }
+        else {
+            ActiveChannel++;
+            //Capture.grab();
+        }
+    }
+
+    void CaptureThread() {
+        int CurrentChannel = 0;
+
+        while (ThreadEnabled) {
+
+            // if active channel has switched
+            if (ActiveChannel != CurrentChannel) {
+
+                // broadcast active channel
+                switch (ActiveChannel)
+                {
+                case 1:
+                    // channel one is broadcast from webcam
+                    Capture.open(0);
+                    break;
+                case 2:
+                    // channel two is broadcast from video file
+                    Capture.open("28.mp4");
+                    break;
+                default:
+                    Capture.release();
+                }
+
+                // set current channel to active channel
+                CurrentChannel = ActiveChannel;
+            }
+
+            // obtain frame from channel
+            if (Capture.isOpened()) {
+                Capture >> CapturedImage;
+            }
+        }
+
+        // release capture
+        if (Capture.isOpened()) {
+            Capture.release();
+        }
+
+    }
+};
+
 
 int main()
 {
-	return Application::Instance()->render();
+    if (glfwInit() == GL_FALSE) return 0;
+    // glfw: initialize and configure
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw window creation
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    if (GLenum err = glewInit()) return 0;
+
+    // build and compile our shader zprogram
+    // ------------------------------------
+    Shader ourShader("assets/shaders/practica.vert", "assets/shaders/practica.frag");
+
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    float vertices[] = {
+        // positions          // colors           // texture coords
+         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
+    };
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    // texture coord attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+
+    
+
+
+    CaptureClient* captureClient = new CaptureClient();
+    std::thread ct(&CaptureClient::CaptureThread, captureClient);
+
+
+
+     unsigned int texture1;
+    // texture 1
+    // ---------
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // load image, create texture and generate mipmaps
+   
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, CapturedImage.cols, CapturedImage.rows, 0, GL_BGR, GL_UNSIGNED_BYTE, CapturedImage.ptr());
+
+  
+    // tell opengl for each sampler to which texture unit it belongs to (only has to be done once)
+    // -------------------------------------------------------------------------------------------
+    ourShader.use(); // don't forget to activate/use the shader before setting uniforms!
+    // either set it manually like so:
+    glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        // -----
+        processInput(window);
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // bind textures on corresponding texture units
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+
+        glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+
+
+        
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, CapturedImage.cols, CapturedImage.rows, 0, GL_BGR, GL_UNSIGNED_BYTE, CapturedImage.ptr());
+            captureClient->SwitchChannel();
+            captureClient->SwitchChannel();
+
+        
+        
+
+        ourShader.use();
+        // render container
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    captureClient->ThreadEnabled = false;
+    ct.join();
+    delete captureClient;
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
 }
 
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // make sure the viewport matches the new window dimensions; note that width and 
+    // height will be significantly larger than specified on retina displays.
+    glViewport(0, 0, width, height);
+}
+
+//#include "Application.h"
+//int main()
+//{
+//	return Application::Instance()->render();
+//}
